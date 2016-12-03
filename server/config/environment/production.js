e0f01e856975c7c991226eeadea6b8df4ddfa680
SIIO <shane.walsh@webelevate.ie>
2016-12-03 19:07:02 +0000

    URI change

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -15,10 +15,9 @@ module.exports = {
 
   // MongoDB connection options
   mongo: {
-    uri:    'mongodb://teamorbit:1dftcdhd4i8bodfegc6p4gp3v0@ds117348.mlab.com:17348/heroku_mx279h6b'
-            //'mongodb://teamorbit:teamorbit@ds117348.mlab.com:17348/heroku_mx279h6b' ||
+    uri:    'mongodb://teamorbit:teamorbit@ds117348.mlab.com:17348/heroku_mx279h6b' ||
             //process.env.MONGOLAB_URI ||
-            //process.env.MONGODB_URI ||
+            process.env.MONGODB_URI
             //process.env.MONGOHQ_URL ||
             //process.env.OPENSHIFT_MONGODB_DB_URL+process.env.OPENSHIFT_APP_NAME ||
             //'mongodb://localhost/tttime' 
