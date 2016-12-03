f63a8cb771c32c3eecff022fe480886d0fef0fc0
SIIO <shane.walsh@webelevate.ie>
2016-12-03 18:57:20 +0000

    Config URI

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -15,12 +15,13 @@ module.exports = {
 
   // MongoDB connection options
   mongo: {
-    uri:    'mongodb://teamorbit:teamorbit@ds117348.mlab.com:17348/heroku_mx279h6b' ||
-            process.env.MONGOLAB_URI ||
-            process.env.MONGODB_URI ||
-            process.env.MONGOHQ_URL ||
-            process.env.OPENSHIFT_MONGODB_DB_URL+process.env.OPENSHIFT_APP_NAME ||
-            'mongodb://localhost/tttime' 
+    uri:    'mongodb://heroku_mx279h6b:1dftcdhd4i8bodfegc6p4gp3v0@ds117348.mlab.com:17348/heroku_mx279h6b'
+            //'mongodb://teamorbit:teamorbit@ds117348.mlab.com:17348/heroku_mx279h6b' ||
+            //process.env.MONGOLAB_URI ||
+            //process.env.MONGODB_URI ||
+            //process.env.MONGOHQ_URL ||
+            //process.env.OPENSHIFT_MONGODB_DB_URL+process.env.OPENSHIFT_APP_NAME ||
+            //'mongodb://localhost/tttime' 
 
   }
 };
