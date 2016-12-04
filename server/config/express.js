2406edf50e7362b9172f41ba117e621a6e69b518
SIIO <shane.walsh@webelevate.ie>
2016-12-04 22:54:14 +0000

    Updated

--- server/config/express.js
+++ server/config/express.js
@@ -28,12 +28,12 @@ module.exports = function(app) {
   app.use(methodOverride());
   app.use(cookieParser());
   app.use(passport.initialize());
-  /*if ('production' === env) {
+  if ('production' === env) {
     app.use(favicon(path.join(config.root, 'public', 'favicon.ico')));
     app.use(express.static(path.join(config.root, 'public')));
     app.set('appPath', config.root + '/public');
     app.use(morgan('dev'));
-  }*/
+  }
 
   if ('development' === env || 'test' === env) {
     app.use(require('connect-livereload')());
